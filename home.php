dd172330c47b6ac93c4bb1723ffd31c9f1ca54e0
ars1224 <a.tayao1224@gmail.com>
2023-05-22 20:45:10 +1200

    home page starts

---
+++ home.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <meta charset="=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MyStock | Sign Up</title>
+        <link rel="stylesheet" href="styles_home.css">
+        
+</head>
+
+<body>
+    <nav class="head">
+        <h2 class="tag" href="home.php">My Stock!</h2>
+        
+        <div class="navi">
+
+            <div class="list">
+                <ul class="home"><a href="home.php">Home</a></ul>
+                <ul class="stock"><a href="home.php">Stock</a></ul>
+                <ul class="received"><a href="home.php">Received Items</a></ul>
+                <ul class="log"><a href="home.php">Log</a></ul>
+            </div>   
+
+            <div class="user">
+                <i class="fa-regular fa-user"></i>
+            </div>
+        </div>
+    </nav>
+    <script src="https://kit.fontawesome.com/5c09730e7a.js" crossorigin="anonymous"></script>
+</body>
+</html>
