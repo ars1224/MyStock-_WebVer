d746c3661e240c66043a2ed8044b4a8ef149858d
ars1224 <a.tayao1224@gmail.com>
2023-05-23 15:21:00 +1200

    added soe nots for my memory

--- home.php
+++ home.php
@@ -24,8 +24,36 @@
             <div class="user">
                 <i class="fa-regular fa-user"></i>
             </div>
-        </div>
     </nav>
+    <div class="home-content">
+        <div class="search-bar">
+            <div class="search">
+                <input class="searchBar" type="text" name="search" placeholder="Looking for..." >
+            </div>
+            
+            <div id="textbx" class="lookingFr">
+                <select name="find" class="resizedtextbox" id="drop">
+                    <option value=""disabled selected hidden>Department</option>
+                    <option value="Factory">Factory</option>
+                    <option value="Warehouse">Warehouse</option>
+                    <option value="Compounding">Compounding</option>
+                </select>
+            </div>
+            <div class="search-Btn">
+                <button class="searchBtn" type="submit" name="searchBtn">Search</button>
+            </div>
+        </div>
+        
+        <div class="tables">
+            <table class="panels">
+                <a href="#"><ul class="products">Products<br></ul></a>
+                <a href="#"><ul class="orders">Orders<br></ul></a>
+                <a href="#"><ul class="Receive">Received Items</ul></a>
+                <a href="#"><ul class="expired">Expired Products</ul></a>
+            </table>
+        </div>
+    </div>
+
     <script src="https://kit.fontawesome.com/5c09730e7a.js" crossorigin="anonymous"></script>
 </body>
-</html>
+</html
