f7fcf49ba8d04cd2ca382ad535c011055e44a268
ars1224 <a.tayao1224@gmail.com>
2023-05-27 23:15:16 +1200

    asdasasasd

--- StockScript.js
+++ StockScript.js
@@ -1,16 +1,5 @@
-
-
-function toggle(){
-    var open_Pop = document.getElementById("PopUp_1");
-    open_Pop.classList.toggle('active')
-
-};
     function toggle(){
         var blurr= document.getElementById("blur");
         blurr.classList.toggle('active')
 
-    };
-    function toggle(){
-            var open_pop = document.getElementById("popUP_2");
-            open_pop.classList.toggle('active')
-        };
+    }
