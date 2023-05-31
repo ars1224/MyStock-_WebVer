839b50474a2c04ee368315489cf22e72b3eb2e36
ars1224 <a.tayao1224@gmail.com>
2023-05-31 15:32:35 +1200

    KHKH

--- StockScript.js
+++ StockScript.js
@@ -7,16 +7,3 @@ function toggle(){
         hide.classList.toggle('hide');
 }
 
-function changeDateFormat() {
-  // Get the input value from the text box
-  var inputDate = document.getElementById("dateInput").value;
-  
-  // Create a Date object using the input value
-  var date = new Date(inputDate);
-  
-  // Create the desired date format
-  var outputDate = date.toISOString().split('T')[0];
-  
-  // Set the formatted date as the new value in the text box
-  document.getElementById("dateInput").value = outputDate;
-}
